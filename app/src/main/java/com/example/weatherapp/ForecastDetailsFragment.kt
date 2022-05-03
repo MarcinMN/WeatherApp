a2e3e2f7ae9c78f7107a99df9199f059fdb99237
MarcinMN <79340820+MarcinMN@users.noreply.github.com>
2022-05-02 19:40:43 -0500

    Assignment 9 - Forecast Details Screen text layout complete. Still can't load the condition icon.

--- app/src/main/java/com/example/weatherapp/ForecastDetailsFragment.kt
+++ app/src/main/java/com/example/weatherapp/ForecastDetailsFragment.kt
@@ -4,26 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Divider
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import coil.ImageLoader
+import coil.request.ImageRequest
+import coil.request.SuccessResult
+import com.bumptech.glide.Glide
 
 class ForecastDetailsFragment : Fragment() {
 
     private val args: ForecastDetailsFragmentArgs by navArgs()
-    //private var forecast: Forecast = args.forecastArg!!
-    //private var forecastList: List<DayForecast> = forecast.list
-    //private var position: Int = args.positionArg
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,63 +37,76 @@ class ForecastDetailsFragment : Fragment() {
         val view = ComposeView(requireContext())
         view.apply {
             setContent {
-                //ScreenLayout(forecastList, position)
                 ScreenLayout()
             }
         }
         return view
     }
-/*
-    @Composable
-    fun ScreenLayout(forecastList : List<DayForecast>, position: Int) {
-
-        Column(modifier = Modifier.padding(16.dp)) {
-            Text("City", fontSize = 30.sp, modifier = Modifier.padding(16.dp))
-            Row {
-                Text("Icon", modifier = Modifier.padding(16.dp))
-                Text("Description", modifier = Modifier.padding(16.dp))
-            }
-            Text(forecastList[position].temp.day.toInt().toString(), modifier = Modifier.padding(16.dp))
-            Row {
-                Text(forecastList[position].temp.max.toInt().toString(), modifier = Modifier.padding(16.dp))
-                Text(forecastList[position].temp.min.toInt().toString(), modifier = Modifier.padding(16.dp))
-            }
-            Row {
-                Text(forecastList[position].humidity.toString(), modifier = Modifier.padding(16.dp))
-                Text(forecastList[position].pressure.toString(), modifier = Modifier.padding(16.dp))
-                Text(forecastList[position].speed.toString(), modifier = Modifier.padding(16.dp))
-            }
-        }
-    }
-
-    @Preview(showSystemUi = true, showBackground = true)
-    @Composable
-    fun PreviewMessage() {
-        ScreenLayout(forecastList, position)
-    }  */
 
     @Composable
     fun ScreenLayout() {
 
-        var forecastList : List<DayForecast> = args.forecastArg!!.list
-        var position : Int = args.positionArg
+        val forecastList : List<DayForecast> = args.forecastArg!!.list
+        val position : Int = args.positionArg
+        val iconName = forecastList[position].weather.firstOrNull()?.icon
+        val weatherDescription = forecastList[position].weather.firstOrNull()?.description
+        val iconUrl = "https://openweathermap.org/img/wn/${iconName}@2x.png"
 
         Column(modifier = Modifier.padding(16.dp)) {
-            Text("City", fontSize = 30.sp, modifier = Modifier.padding(16.dp))
+            Text("Icon", modifier = Modifier.padding(16.dp))
+            Text(
+                weatherDescription!!,
+                fontSize = 18.sp,
+                modifier = Modifier.padding(16.dp)
+            )
             Row {
-                Text("Icon", modifier = Modifier.padding(16.dp))
-                Text("Description", modifier = Modifier.padding(16.dp))
+                Text(getString(R.string.forecast_details_temp, forecastList[position].temp.day.toInt().toString()),
+                    fontSize = 72.sp,
+                    modifier = Modifier.padding(16.dp)
+                )
+                Column {
+                    Text(
+                        getString(
+                            R.string.forecast_high,
+                            forecastList[position].temp.max.toInt().toString()
+                        ),
+                        fontSize = 18.sp,
+                        modifier = Modifier.padding(16.dp, 38.dp, 16.dp, 8.dp)
+                    )
+                    Text(
+                        getString(
+                            R.string.forecast_low,
+                            forecastList[position].temp.min.toInt().toString()
+                        ),
+                        fontSize = 18.sp,
+                        modifier = Modifier.padding(16.dp, 0.dp, 16.dp, 8.dp)
+                    )
+                }
             }
-            Text(forecastList[position].temp.day.toInt().toString(), modifier = Modifier.padding(16.dp))
-            Row {
-                Text(forecastList[position].temp.max.toInt().toString(), modifier = Modifier.padding(16.dp))
-                Text(forecastList[position].temp.min.toInt().toString(), modifier = Modifier.padding(16.dp))
-            }
-            Row {
-                Text(forecastList[position].humidity.toString(), modifier = Modifier.padding(16.dp))
-                Text(forecastList[position].pressure.toString(), modifier = Modifier.padding(16.dp))
-                Text(forecastList[position].speed.toString(), modifier = Modifier.padding(16.dp))
-            }
+            Text(
+                getString(
+                    R.string.humidity,
+                    forecastList[position].humidity
+                ),
+                fontSize = 18.sp,
+                modifier = Modifier.padding(16.dp, 16.dp, 16.dp, 8.dp)
+            )
+            Text(
+                getString(
+                    R.string.pressure,
+                    forecastList[position].pressure.toInt()
+                ),
+                fontSize = 18.sp,
+                modifier = Modifier.padding(16.dp, 0.dp, 16.dp, 8.dp)
+            )
+            Text(
+                getString(
+                    R.string.wind_speed,
+                    forecastList[position].speed.toString()
+                ),
+                fontSize = 18.sp,
+                modifier = Modifier.padding(16.dp, 0.dp, 16.dp, 8.dp)
+            )
         }
     }
 
